189c06bc9b40a2eff64576414ef0088f569e21cd
arnovanhilten@gmail.com <arnovanhilten@gmail.com>
2026-01-19 11:08:11 +0100

    update with wrapper

--- run_depict.sh
+++ run_depict.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+#test
+
 # Runtime workspace (must be writable)
 export DEPICT_RUN=/userdata
 export TMPDIR=${DEPICT_RUN}/tmp
@@ -12,5 +14,6 @@ mkdir -p "$DEPICT_RUN/results"
 # Force DEPICT to use writable paths
 export DEPICT_BACKGROUND_DIR="$DEPICT_RUN/backgrounds"
 export DEPICT_GENOTYPE_DIR="$DEPICT_RUN/genotype_data_plink"
+
 
 exec /opt/depict/src/python/depict.py "$@"
